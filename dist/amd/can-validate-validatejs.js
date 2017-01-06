bf3318b01d76d629f09ad678315bdc6858f46d9a
Juan Orozco <hi@juanorozco.com>
2017-01-06 18:54:24 -0500

    0.0.2-0

---
+++ dist/amd/can-validate-validatejs.js
@@ -0,0 +1,17 @@
+/*can-validate-validatejs@0.0.1#can-validate-validatejs*/
+define(function (require, exports, module) {
+    var OldValidate = require('can-validate');
+    var validatejs = require('validate');
+    var shim = {
+        test: function () {
+            return validatejs.single.apply(validatejs, arguments);
+        }
+    };
+    var NewValidate = OldValidate.extend({
+        init: function () {
+            this.registerLibrary('validate.js', shim);
+        }
+    });
+    module.exports = NewValidate;
+});
+//# sourceMappingURL=can-validate-validatejs.js.map
