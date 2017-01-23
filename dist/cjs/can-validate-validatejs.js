d6e4090d9ef8294633608508811aa8d6c38a1857
Juan Orozco <hi@juanorozco.com>
2017-01-23 13:47:13 -0500

    0.0.3

---
+++ dist/cjs/can-validate-validatejs.js
@@ -0,0 +1,29 @@
+/*can-validate-validatejs@0.0.2#can-validate-validatejs*/
+var each = require('can-util/js/each/each');
+var validatejs = require('validate.js');
+var makeValidator = function (constraints) {
+    return function (value) {
+        return validatejs.single(value, constraints);
+    };
+};
+makeValidator.many = function (constraints) {
+    return function (values) {
+        var rawErrors = validatejs(values, constraints, {
+            format: 'detailed',
+            fullMessages: false
+        });
+        var errors;
+        if (rawErrors) {
+            errors = [];
+            each(rawErrors, function (error) {
+                errors.push({
+                    message: error.options.message || error.error,
+                    related: [error.attribute]
+                });
+            });
+        }
+        return errors;
+    };
+};
+module.exports = makeValidator;
+//# sourceMappingURL=can-validate-validatejs.js.map
