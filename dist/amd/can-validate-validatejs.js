40d5cd099eaf222ef8ac5c4b490a32f5e8b393fa
Juan Orozco <hi@juanorozco.com>
2017-04-06 15:00:29 -0400

    0.0.5-0

---
+++ dist/amd/can-validate-validatejs.js
@@ -0,0 +1,32 @@
+/*can-validate-validatejs@0.0.4#can-validate-validatejs*/
+define(function (require, exports, module) {
+    var each = require('can-util/js/each');
+    var validatejs = require('validate');
+    var makeValidator = function (constraints) {
+        return function (value) {
+            return validatejs.single(value, constraints);
+        };
+    };
+    makeValidator.many = function (constraints) {
+        return function (values) {
+            var rawErrors = validatejs(values, constraints, {
+                format: 'detailed',
+                fullMessages: false
+            });
+            var errors;
+            if (rawErrors) {
+                errors = [];
+                each(rawErrors, function (error) {
+                    errors.push({
+                        message: error.options.message || error.error,
+                        related: [error.attribute]
+                    });
+                });
+            }
+            return errors;
+        };
+    };
+    exports.validatejs = validatejs;
+    module.exports = makeValidator;
+});
+//# sourceMappingURL=can-validate-validatejs.js.map
