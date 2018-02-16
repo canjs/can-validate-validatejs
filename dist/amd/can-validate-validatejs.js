d9ad92def45f0c4050661215001334831e313a63
Christopher Baker <christopher@bitovi.com>
2018-02-16 11:35:57 -0800

    0.1.3

---
+++ dist/amd/can-validate-validatejs.js
@@ -0,0 +1,38 @@
+/*can-validate-validatejs@0.1.2#can-validate-validatejs*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-reflect',
+    'validate'
+], function (require, exports, module) {
+    var canReflect = require('can-reflect');
+    var validatejs = require('validate');
+    var makeValidator = function (constraints) {
+        return function (value) {
+            return validatejs.single(value, constraints);
+        };
+    };
+    makeValidator.many = function (constraints) {
+        return function (values) {
+            var rawErrors = validatejs(values, constraints, {
+                format: 'detailed',
+                fullMessages: false
+            });
+            var errors;
+            if (rawErrors) {
+                errors = [];
+                canReflect.eachIndex(rawErrors, function (error) {
+                    errors.push({
+                        message: error.options.message || error.error,
+                        related: [error.attribute]
+                    });
+                });
+            }
+            return errors;
+        };
+    };
+    makeValidator.validatejs = validatejs;
+    module.exports = makeValidator;
+});
+//# sourceMappingURL=can-validate-validatejs.js.map
